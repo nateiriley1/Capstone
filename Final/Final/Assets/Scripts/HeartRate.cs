1bea767d1b933345216f6d964919a85812f55a83
nr7195a <nr7195a@student.american.edu>
2020-04-26 21:03:05 -0400

    Heart Rate Sensor

--- Final/Final/Assets/Scripts/HeartRate.cs
+++ Final/Final/Assets/Scripts/HeartRate.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class HeartRate : MonoBehaviour
 {
+
+    public float alphaLevel = 0.5f;
+
+    bool changeBool = true;
+
     // Start is called before the first frame update
     void Start()
     {
+
+        float HR = 150;
+
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (changeBool == true) {
+            StartCoroutine(HeartRateChange());
+            float change = Random.Range(-0.01f, 0.01f);
+            Debug.Log(change);
+            alphaLevel = alphaLevel + change;
+
+            GetComponent<MeshRenderer>().material.color = new Color(1, 1, 1, alphaLevel);
+        }
     }
+
+    IEnumerator HeartRateChange()
+    {
+        changeBool = false;
+        yield return new WaitForSeconds(0.01f);
+        changeBool = true;
+
+    }
+
 }
